b868d9037de55d3fbf7ee36dc2353849d88abe87
Sh1617 <22f3001953@ds.study.iitm.ac.in>
2025-06-29 07:39:22 +0000

    updated test_model.py

--- test_model.py
+++ test_model.py
@@ -1,13 +1,49 @@
+
+
+
+
 import unittest
 import joblib
 import numpy as np
 
 class TestModel(unittest.TestCase):
     def test_prediction_shape(self):
-        model = joblib.load("models/model.pkl")  # update path if needed
+        model = joblib.load("models/decision_tree_model.pkl")  # update path if needed
         sample = np.array([[5.1, 3.5, 1.4, 0.2]])  # dummy sample
         prediction = model.predict(sample)
         self.assertEqual(len(prediction), 1)
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
